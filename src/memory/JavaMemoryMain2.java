15bd62d5c7f656e490b9b8062965dcda0941466a
ParkKyungMin <pecobee813@huniverse.co.kr>
2025-01-07 09:01:00 +0900

    섹션8_static 변수2_250107

--- src/memory/JavaMemoryMain2.java
+++ src/memory/JavaMemoryMain2.java
@@ -2,7 +2,8 @@ package memory;
 
 /**
  * 자바메모리 - 스택 영역과 힙 영역
- *
+ * 1.스택 영역 : 지역변수 - 프레임이 사라지면 지역변수 제거
+ * 2.힙 영역 : 인스턴스 - 참조되는 부분이 없으면 GC 대상
  */
 public class JavaMemoryMain2 {
     public static void main(String[] args) { //main()을 위한 스택 프레임 생성
@@ -14,7 +15,7 @@ public class JavaMemoryMain2 {
     static void method1() {
         System.out.println("method1 start");
         Data data1 = new Data(10);
-        method2(data1);
+        method2(data1); //data1 = x001
         System.out.println("method1 end");
     }
 
