ee88f624d92df79424e7de1bfe8e76995f1e4850
박경민 <pecobee813@naver.com>
2025-03-20 22:41:00 +0900

    [중급1]섹션2_Object클래스_문제풀이

---
+++ src/mid1/lang/object/test/Rectangle.java
@@ -0,0 +1,28 @@
+package mid1.lang.object.test;
+
+public class Rectangle {
+    private int width;
+    private int height;
+
+    public Rectangle(int a, int b) {
+        this.width = a;
+        this.height = b;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "a=" + width +
+                ", b=" + height +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        Rectangle rectangle = (Rectangle) object;
+        return width == rectangle.width && height == rectangle.height;
+    }
+
+}
--- src/mid1/lang/object/test/RectangleMain.java
+++ src/mid1/lang/object/test/RectangleMain.java
@@ -2,11 +2,15 @@ package mid1.lang.object.test;
 
 public class RectangleMain {
     public static void main(String[] args) {
-        Rectangle rect1 = new Rectangle(100,20);
-        Rectangle rect2 = new Rectangle(100,20);
+        Rectangle rect1 = new Rectangle(100,20); //x001
+        Rectangle rect2 = new Rectangle(100,20); //x002
+
+        //
         System.out.println(rect1);
         System.out.println(rect2);
-        System.out.println(rect1 == rect2);
-        System.out.println(rect1.equals(rect2));
+        // 동일성
+        System.out.println(rect1 == rect2); //false
+        // 동등성
+        System.out.println(rect1.equals(rect2)); //true
     }
 }
